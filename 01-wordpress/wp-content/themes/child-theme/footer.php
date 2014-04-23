b24b77873ed43daa0f9f1abe6e105556a1352c9d
dandlezzz <danieladammiller@gmail.com>
2014-04-23 09:51:35 -0700

    updates footer.php in the child theme to include modified by dandlezzz

---
+++ 01-wordpress/wp-content/themes/child-theme/footer.php
@@ -0,0 +1,21 @@
+    <footer id="site-credits" class="container">
+        <div class="row">
+            <div class="col-sm-6 text-muted credit">
+                <p>
+                    All content copyright &copy; 
+                    <a href="<?php echo home_url('/'); ?>" 
+                       rel="home"><?php bloginfo('name', 'display'); ?></a>
+                </p>
+            </div>
+            <div class="col-sm-6 text-muted text-right credit">
+                <p>
+                    Modified by 
+                    <a href="https://github.com/dandlezzz">Daniel Miller</a>
+                </p>
+            </div>
+        </div>
+    </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
